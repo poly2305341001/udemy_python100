6607f4618424b53f713b8fa5e3538902c7cda814
CPHT23-1 <128008396+poly2305341001@users.noreply.github.com>
2023-07-23 19:32:24 +0900

    2일차 - 데이터 형식 이해 및 문자열 조작 방법

---
+++ d002_q_tip_calculator.py
@@ -0,0 +1,18 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+bill = float(input("The bill is? $"))
+people = int(input("How many people? "))
+tip = int(input("How much tip(%)?" ))
+
+each = (bill / people) * (1 + tip*0.01)
+# result = round(each, 2)
+result = "{:.2f}".format(each)
+
+print(result)
